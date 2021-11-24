be748be6927721530208bcb01931d56e7087dfc5
Ruslan <89871727+Lofranlof@users.noreply.github.com>
2021-11-24 13:37:48 +0300

    Update main.c

--- main.c
+++ main.c
@@ -4,8 +4,8 @@ int main()
 {
     int number, number2;
     scanf("%x", &number);
-    printf("%o - %s\n", number, " введенное число в 8 системе счисления");
-    printf("%o, %o - %s\n", number, number << 3, "введенное число с побитовым сдвигом вправо на 3");
+    printf("%x - %s\n", number, " введенное число в 8 системе счисления");//запрос числа происходит в 16-ричной сс
+    printf("%o, %o - %s\n", number, number >> 3, "введенное число с побитовым сдвигом вправо на 3");//побитовый сдвиг происходит влево
     printf("%o, %o - %s\n" , number, ~number, "введенно число с побитовым отрицание");
     scanf("%o", &number2);
     printf("%o - %s", number2^number, "number исключаещее или number2");
