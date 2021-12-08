ed9e2f84feddb6d67728d3ed293e1a58c7a83ef5
Ruslan <89871727+Lofranlof@users.noreply.github.com>
2021-12-08 16:09:44 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
 #include <math.h>
+#include <string>
 
 int main()
 {
-    double a, numenator, denomenator, z1, z2;
-    scanf("%lf", &a);
-    numenator = sin(2 * a) + sin(5 * a) - sin(3 * a);
-    denomenator = cos(a) + 1 - 2 * pow(sin(2 * a), 2);
-    z1 = numenator / denomenator;
-    z2 = 2 * sin(a);
-    printf("%lf\n %lf\n", z1, z2);
+    int var = 0;
+    string greetings = "Asalamu Aleikum";
     return 0;
 }
