086c6a06bec53cc546905c63a627af99d0c8023f
Ruslan <89871727+Lofranlof@users.noreply.github.com>
2021-12-08 13:58:47 +0300

    Update main.c

--- main.c
+++ main.c
@@ -3,15 +3,12 @@
 
 int main()
 {
-    double a, chislitel, znamenatel, z1, z2, z3, z4, z5, z6;
+    double a, numenator, denomenator, z1, z2;
     scanf("%lf", &a);
-    chislitel = sin(2 * a) + sin(5 * a) - sin(3 * a);
-    znamenatel = cos(a) + 1 - 2 * pow(sin(2 * a), 2);
-    z1 = chislitel / znamenatel;
+    numenator = sin(2 * a) + sin(5 * a) - sin(3 * a);
+    denomenator = cos(a) + 1 - 2 * pow(sin(2 * a), 2);
+    z1 = numenator / denomenator;
     z2 = 2 * sin(a);
-    /*z3 = tan(a);
-    z4 = sqrt(a);
-    z5 = log(a);*/
-    printf("lf\n %lf\n %lf\n %lf\n %lf\n", z1, z2 /*z3, z4, z5*/);//Забыл знак % у первого lf
+    printf("%lf\n %lf\n", z1, z2);
     return 0;
 }
